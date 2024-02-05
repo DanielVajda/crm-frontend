4c2b437a7b93912262c599cd1f3c85d593f188fc
DanielVajda <daniel.vajda048@gmail.com>
2024-02-05 17:33:27 +0100

    second commit

---
+++ src/Page/entry/Entry.page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Entry.style.css'
+
+export const Entry = () => {
+  return (
+    <div className='entry-page bg-info'>
+      entry page
+    </div>
+  )
+}
+
+export default Entry.page
